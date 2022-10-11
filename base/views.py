a07abfa1a2a904304a10f2ecea2934f582d9d303
“BillMuriu” <njorogekamau63@gmail.com>
2022-10-11 14:21:58 +0100

    adding the search Functionality

--- base/views.py
+++ base/views.py
@@ -11,7 +11,8 @@ from .forms import RoomForm
 # ]
 
 def home(request):
-    rooms = Room.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(topic__name__icontains=q)
     topic = Topic.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
